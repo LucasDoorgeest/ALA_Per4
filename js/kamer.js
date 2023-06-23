89758d0cf40b4bcb8f56c0317d30fc9ac0cfcb70
Lucas Doorgeest <Lucas.Doorgeest@gmail.com>
2023-06-23 10:37:20 +0200

    updates
    css, js

--- js/kamer.js
+++ js/kamer.js
@@ -19,6 +19,10 @@ if(kamer) {
         setTimeout(tryToSetupStream, (reconnectFrequency * 1000))
     };
 
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
     let send = function(device, feature) {
         fetch('http://192.168.0.100:8000/api/set', {
             method: 'POST',
@@ -42,14 +46,17 @@ if(kamer) {
 
             if (msgTopic == 'devices') {
                 for(const [key, device] of Object.entries(msgPayload)) {
+                    console.log(device.friendly_name);
                     deviceName = device.friendly_name.split("/");
                     if(deviceName[0] == kamer) {
                         if(!document.getElementById(deviceName[1])) {
                             let deviceDiv = document.createElement("div");
+                            deviceDiv.classList.add("perLamp");
                             deviceDiv.id = deviceName[1];
                             
                             let deviceLabel = document.createElement("label")
                             deviceLabel.innerHTML = deviceName[1];
+                            deviceLabel.classList.add("lampLabel");
                             deviceDiv.appendChild(deviceLabel);
 
                             if (device.definition) {
@@ -57,6 +64,7 @@ if(kamer) {
                                     for(const [key2, expose] of Object.entries(device.definition.exposes)) {
                                         if(expose.features) {
                                             for(const [key3, feature] of Object.entries(expose.features)) {
+                                                console.log(feature.name);
                                                 let featureDiv = document.createElement("div"); // Container div met class "feature"
                                                 featureDiv.classList.add("feature");
 
@@ -120,6 +128,25 @@ if(kamer) {
                                                         }
                                                     })
                                                     featureDiv.appendChild(featureButton);
+                                                    
+                                                    let rainbowModeLabel = document.createElement('label');
+                                                    rainbowModeLabel.innerHTML = "RainbowMode";
+                                                    rainbowModeLabel.classList.add("featureLabel");
+                                                    featureDiv  .appendChild(rainbowModeLabel)
+                                                    let rainbowModeButton = document.createElement("button");
+                                                    let hexColorCode = ["#ff0000", "#ffa500", "#ffff00", "#008000", "#0000ff", "#4b0082", "#ee82ee"]
+                                                    rainbowModeButton.type = "color";
+                                                    rainbowModeButton.id = deviceName[1] + "_numeric";
+                                                    rainbowModeButton.classList.add("composite");
+                                                    rainbowModeButton.addEventListener("click", (event) => {
+                                                        for (let hexColor of hexColorCode) {
+                                                            send(device.friendly_name, {
+                                                                "color": hexColor
+                                                            })
+                                                            sleep(1200)
+                                                        }
+                                                    })
+                                                    featureDiv.appendChild(rainbowModeButton);
                                                 }
 
                                                 deviceDiv.appendChild(featureDiv);
